0a316181e1158ad1674a436e78404c14af6bd979
CristianParadaM <104220931+CristianParadaM@users.noreply.github.com>
2022-09-04 10:32:30 -0500

    plantilla spring

---
+++ src/main/java/co/edu/utp/ilovepets/main/ILovePetsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.utp.ilovepets.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILovePetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ILovePetsApplication.class, args);
+	}
+
+}
